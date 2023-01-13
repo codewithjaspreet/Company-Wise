a6cc853b8040af9bde969e8272d0f4980317891c
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-01-13 16:47:14 +0530

    key-pair

---
+++ Microsoft/key_pair.java
@@ -0,0 +1,26 @@
+public class key_pair {
+    
+    public int[] twoSum(int nums[], int target) {
+   
+        
+    HashMap<Integer , Integer> map = new HashMap<>();
+      
+    int[] arr = new int[2];
+      
+    for(int i = 0 ; i < nums.length ; i ++){
+      
+      if(map.containsKey(target - nums[i])){
+       
+        arr[0] = i;
+        arr[1] = map.get(target - nums[i]);
+        return arr;
+      }
+      
+      map.put(nums[i] , i);
+    }
+      
+    return new int[]{};
+      
+    }
+   
+}
