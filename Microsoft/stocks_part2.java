044c470175418108d6d345e26e60310c865db727
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-01-21 19:15:21 +0530

    tab +spaceopti

--- Microsoft/stocks_part2.java
+++ Microsoft/stocks_part2.java
@@ -33,7 +33,7 @@ public class stocks_part2 {
 
     // memoization
 
-    class Solution {
+    class stocks_part2_1 {
         public int maxProfit2(int[] prices) {
 
             int[][] dp = new int[prices.length][2];
@@ -71,9 +71,117 @@ public class stocks_part2 {
         }
     }
 
-    // tabulation
+    class stocks_part2_2{
+
+        public int maxProfit3(int[] prices) {
+
+            int[][] dp = new int[prices.length][2];
+
+            for (int[] row : dp) {
+
+                Arrays.fill(row, -1);
+            }
+
+            return solve3(prices, dp);
+        }
+
+         int solve3(int[] prices, int[][] dp) {
+
+            
+            for (int i = prices.length - 1; i >= 0; i--) {
+
+                for (int buy = 0; buy < 2; buy++) {
+
+                    if (i == prices.length - 1) {
+
+                        dp[i][buy] = 0;
+                        continue;
+                    }
+
+                    int profit = 0;
+
+                    if (buy == 1) {
+
+                        profit += Math.max((-prices[i] + dp[i + 1][0]), (dp[i + 1][1]));
+                    }
+
+                    else {
+
+                        profit += Math.max((prices[i] + dp[i + 1][1]), (dp[i + 1][0]));
+                    }
+
+                    dp[i][buy] = profit;
+                }
+            }
+
+            return dp[0][1];
+
+        }
+
+        public class stocks_part2_3 {
+        
+            // space optimized solution
+
+            public int maxProfit4(int[] prices) {
+
+                int[][] cur = new int[2][2];
+                int[][] prev = new int[2][2];
+
+                for (int[] row : cur) {
+
+                    Arrays.fill(row, -1);
+                }
+
+                for (int[] row : prev) {
+
+                    Arrays.fill(row, -1);
+                }
+
+                for (int i = prices.length - 1; i >= 0; i--) {
+
+                    for (int buy = 0; buy < 2; buy++) {
+
+                        if (i == prices.length - 1) {
+
+                            cur[buy][0] = 0;
+                            continue;
+                        }
+
+                        int profit = 0;
+
+                        if (buy == 1) {
+
+                            profit += Math.max((-prices[i] + prev[0][0]), (prev[1][0]));
+                        }
+
+                        else {
+
+                            profit += Math.max((prices[i] + prev[1][0]), (prev[0][0]));
+                        }
+
+                        cur[buy][0] = profit;
+                    }
+
+                    prev = cur;
+                    cur = new int[2][2];
+                }
+
+                return prev[1][0];
+
+                
+            }
+
+             
+        }
+    }
 
     
+
+    
+
+    // tabulation
+
+
 
 
 
