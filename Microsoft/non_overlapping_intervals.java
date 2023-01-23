af35dd7c46a18b264d92ed9de682caada7ca97db
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-01-23 21:13:50 +0530

    end-point-sort

---
+++ Microsoft/non_overlapping_intervals.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class non_overlapping_intervals {
+    
+
+    // algorithm: sort on end point
+
+    // why we sort on end point - because we want to remove the interval with the
+    // highest end point
+
+       
+    
+    }
