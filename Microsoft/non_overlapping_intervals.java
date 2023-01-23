92c9700c5a452ed09d443088bba1a5f38639ffb4
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-01-23 21:15:05 +0530

    end-point-sort

--- Microsoft/non_overlapping_intervals.java
+++ Microsoft/non_overlapping_intervals.java
@@ -7,7 +7,31 @@ public class non_overlapping_intervals {
 
     // why we sort on end point - because we want to remove the interval with the
     // highest end point
-
-       
     
+        public int eraseOverlapIntervals(int[][] intervals) {
+
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+            int start = intervals[0][0];
+            int end = intervals[0][1];
+
+            int count = 0;
+
+            for (int i = 1; i < intervals.length; i++) {
+
+                if (intervals[i][0] < end) {
+
+                    count++;
+                }
+
+                else {
+
+                    start = intervals[i][0];
+                    end = intervals[i][1];
+                }
+            }
+
+            return count;
+
+        }
     }
