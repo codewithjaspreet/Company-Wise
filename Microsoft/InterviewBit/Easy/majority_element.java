8b5510e7c4d3150d18f382c43df70381172d76bb
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-03-25 18:16:45 +0530

    moore-voting

--- Microsoft/InterviewBit/Easy/majority_element.java
+++ Microsoft/InterviewBit/Easy/majority_element.java
@@ -14,7 +14,7 @@ public class Solution {
         }
         
         
-        
+
         for(int  a : map.keySet()){
             
             if(map.get(a) > A.length / 2){
@@ -25,5 +25,39 @@ public class Solution {
         }
         
         return ans;
+    }
+
+    public static int majorityElement2(int[] arr){
+
+        int ansIndex  = 0;
+        int count = 1;
+
+        for(int i = 1; i < arr.length ; i++){
+
+            if(arr[ansIndex] == arr[i]){
+
+                count++;
+            }
+            else count--;
+
+            if(count == 0){
+
+                ansIndex = i;
+                count = 1;
+            }
+        }
+
+        int temp = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+
+            if(arr[i] == arr[ansIndex]){
+
+                temp++;
+            }
+        }
+
+        if(temp > arr.length / 2) return arr[ansIndex];
+        else return -1;
+
     }
 }
